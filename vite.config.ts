876ab36b6f3006aef7570b5139834ed9e5e12826
Mehedi Hasan <asmaparvin019@gmail.com>
2024-12-06 19:26:55 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Adjust the base URL if your app is hosted in a subdirectory
-})
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
